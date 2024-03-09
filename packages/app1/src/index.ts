2ff3c4b2c5d51b224c4641ce1b19683995c85f05
darkruby <art@darkruby.co.uk>
2024-03-09 15:31:36 +0000

    basic func

--- packages/app1/src/index.ts
+++ packages/app1/src/index.ts
@@ -1,3 +1,55 @@
-import msg1 from "@darkruby/fp-express";
+import {
+  ErrorType,
+  createRequestHandler,
+  type AppError,
+  type HandlerTask,
+} from "@darkruby/fp-express";
+import bodyParser from "body-parser";
+import { Database } from "bun:sqlite";
+import { default as express } from "express";
+import * as TE from "fp-ts/TaskEither";
+import { pipe } from "fp-ts/lib/function";
 
-console.log(msg1);
+// usage here...
+type User = { id: number; name: string };
+
+const createUser: HandlerTask<User, Database> = ({
+  handlerParams: [req],
+  context: db,
+}) =>
+  TE.tryCatch<AppError, User>(
+    async () => {
+      const user: User = req.body;
+      db.exec(`insert into 'user' values(?, ?)`, [user.id, user.name]);
+      return user;
+    },
+    (err) => ({ type: ErrorType.BadRequest, error: (err as Error).message })
+  );
+
+const getUserById: HandlerTask<User, Database> = ({
+  handlerParams: [req],
+  context: db,
+}) =>
+  TE.tryCatch(
+    async () =>
+      db.query(`select * from 'user' where id=?`).get(req.params.id) as User,
+    (err) => ({ type: ErrorType.General, error: (err as Error).message })
+  );
+
+const app = express();
+app.use(bodyParser.json());
+
+const db = new Database();
+
+const createUserHandler = pipe(createUser, createRequestHandler(db));
+const userByIdHandler = pipe(getUserById, createRequestHandler(db));
+
+db.exec("create table user (id int primary key, name string not null)");
+// db.exec("insert into user values(123, 'art')");
+
+app.post("/api/user/", createUserHandler);
+app.get("/api/user/:id", userByIdHandler);
+
+app.listen(8099, () => console.log("listening"));
+
+// curl -X POST -H "Content-Type: application/json" -d '{"name": "art", "id": "123"}' http://localhost:8099/api/user/
--- packages/fp-express/src/index.ts
+++ packages/fp-express/src/index.ts
@@ -1,9 +1,80 @@
-import { map, some } from "fp-ts/Option";
+import type { RequestHandler } from "express";
+import * as RT from "fp-ts/ReaderTask";
+import * as RTE from "fp-ts/ReaderTaskEither";
+import * as I from "fp-ts/lib/Identity";
+import * as T from "fp-ts/lib/Task";
 import { pipe } from "fp-ts/lib/function";
 
-const aaa = pipe(
-  some(123),
-  map((x) => x * 2)
-);
+export enum ErrorType {
+  NotFound = "NotFound",
+  BadRequest = "BadRequest",
+  General = "General",
+  Skip = "Skip",
+}
 
-export default aaa;
+export type AppError = {
+  type: ErrorType;
+  error: string;
+};
+
+export type HandlerContext<Ctx> = {
+  handlerParams: Parameters<RequestHandler>;
+  context: Ctx;
+};
+
+export type HandlerTask<T, Ctx = unknown> = RTE.ReaderTaskEither<
+  HandlerContext<Ctx>,
+  AppError,
+  T
+>;
+
+export type ErrorHandler<Ctx> = (
+  err: AppError
+) => RT.ReaderTask<HandlerContext<Ctx>, unknown>;
+
+export const errorHandler: ErrorHandler<unknown> =
+  (err) =>
+  ({ handlerParams: [_, res, next] }) =>
+    T.fromIO(() => {
+      console.error([err.type, err.error]);
+      switch (err.type) {
+        case ErrorType.BadRequest:
+          return res.status(400).send(err.error);
+        case ErrorType.NotFound:
+          return res.status(404).send(err.error);
+        case ErrorType.Skip:
+          return next();
+        case ErrorType.General:
+        default:
+          return res.status(500).send(err.error);
+      }
+    });
+
+export type SuccessHandler<Ctx> = <T>(
+  data: T
+) => RT.ReaderTask<HandlerContext<Ctx>, unknown>;
+
+export const successHandler: SuccessHandler<unknown> =
+  <T>(data: T) =>
+  ({ handlerParams: [_, res] }) =>
+    T.fromIO(() => {
+      console.info(["ok"]);
+      res.status(200).send(data);
+    });
+
+export const createRequestHandler =
+  <T, Ctx>(context: Ctx) =>
+  (task: HandlerTask<T, Ctx>): RequestHandler =>
+  (req, res, next) =>
+    pipe(
+      task,
+      RTE.filterOrElse<AppError, T>(
+        (x) => x !== null || x != undefined,
+        () => ({ type: ErrorType.NotFound, error: `not found` })
+      ),
+      RTE.fold<HandlerContext<Ctx>, AppError, T, unknown>(
+        errorHandler,
+        successHandler
+      ),
+      I.ap<HandlerContext<Ctx>>({ handlerParams: [req, res, next], context })
+    )();
